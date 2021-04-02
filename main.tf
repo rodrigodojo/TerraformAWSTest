d2a828aa6be263792da76955cbd202b537cadc83
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-04-02 13:31:05 -0300

    testando o terraform e criando aruivo variables.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
  # Configure the AWS Provider
  provider "aws"{
-     access_key = "AKIAYXIFZ3QKE6NXHCDO"
-     secret_key = "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"
-     region = "us-east-1"
+     access_key = "${var.access_key}"
+     secret_key = "${var.secret_key}"
+     region = "${var.region}"
  }
 
 resource "aws_s3_bucket" "b" {
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region"{
+    default = "us-east-1"
+    description = "Main region"
+}
+variable "access_key"{
+    default = "AKIAYXIFZ3QKE6NXHCDO"
+}
+variable "secret_key"{
+    default = "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"
+}
